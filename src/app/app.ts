5360b46e8b5e83fa1ec531f38d2d4049f0c63729
Oyegbami Mary Peace <oyegbamimary5@gmail.com>
2026-03-25 21:26:58 -0700

    second angular commit

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterModule],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
+})
+export class App {
+  protected readonly title = signal('gym-app');
+}
